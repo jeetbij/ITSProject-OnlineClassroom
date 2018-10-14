6b80e6d2abe9effe44e53255ee5f228c5c9bcc72
Garvit Kataria <garvit.k16@iiits.in>
2018-10-14 12:21:46 +0530

    Poll Module: Add Poll and Poll Options.

--- Polls/views.py
+++ Polls/views.py
@@ -34,4 +34,59 @@ class PollView(APIView):
 				"error": "Classroom query for polls doesn't exists."
 				}, status=status.HTTP_400_BAD_REQUEST)
 	
+# 	type 1: Add Poll
+# 	{
+# 	"type":1,
+# 	"classroom_id":1,
+# 	"poll_text":"Which Bike",
+# 	}
+
+# 	type 2: Add PollOptions
+# 	{
+# 		"type":2,
+# 		"parent_poll_id": 5,
+# 		"poll_option_text":"KTM 2"
+# 	}
+
+
+	def post(self, request, format=None):
+		try:
+			query_type = request.data.get('type')
+			if(query_type==1):
+				classroom_id = request.data.get('classroom_id')
+				classroom = Classroom.objects.get(id=classroom_id)
+				poll = Poll()
+				poll.creater = request.user
+				poll.classroom = classroom
+				poll.poll_text = request.data.get('poll_text')
+				poll.save()
+				print(poll)
+				poll_serialized = PollSerializer(poll,many=False).data
+				return Response(poll_serialized)
+			elif(query_type==2):
+				parrent_poll_id = request.data.get('parent_poll_id')
+				poll_option = PollOption()
+				poll_option.parrent_poll = Poll.objects.get(id=parrent_poll_id)
+				poll_option.option_text = request.data.get('poll_option_text')
+				poll_option.save()
+				print(poll_option)
+				poll_option_serialized = PollOptionSerializer(poll_option,many=False).data
+				return Response(poll_option_serialized)
+			return Response({
+				"error": "Type value is not defined."
+			}, status=status.HTTP_400_BAD_REQUEST)
+
+		except Exception as e:
+			return Response({
+				"error": "Classroom query for polls doesn't exists."
+			}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
 		
